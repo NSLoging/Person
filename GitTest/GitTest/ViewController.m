70fe298a83e6125ea53e05fce4b40eb8a08b02f8
ouyangsuhao <lanouhn@dufangyi.local>
2016-06-20 16:43:25 +0800

    哈哈哈哈哈  我也来了

--- GitTest/GitTest/ViewController.m
+++ GitTest/GitTest/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"哥们儿来了");
 }
 
 - (void)didReceiveMemoryWarning {
